8d66d243887e756c56b14009e9338682adbe67d3
Kirtley Adams <hello@kirtleymichelle.com>
2021-12-30 23:00:31 -0500

    html and css

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,7 @@
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse1'))
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
+$('#collapseExample2').on('show.bs.collapse', function () {
+  alert();
+});
